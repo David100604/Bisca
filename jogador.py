c3d69cc997ad8c1ef7e828f9353668234b7c15ae
David100604 <114587007+David100604@users.noreply.github.com>
2023-08-17 10:16:14 -0300

    Update jogador.py

--- jogador.py
+++ jogador.py
@@ -6,6 +6,19 @@ class Jogador:
         self.nome = nome
         self.mao = []
 
+    def Jogar(self):
+        print(f"{self.nome}, é a sua vez de jogar.")
+        print("Suas cartas:", self.mao)
 
+        carta_jogada = input("Escolha uma carta para jogar (digite o índice da carta na sua mão): ")
 
+        try:
+            carta_jogada_index = int(carta_jogada) - 1
+            carta_jogada = self.mao[carta_jogada_index]
+            self.mao.pop(carta_jogada_index)
+            print(f"{self.nome} jogou a carta: {carta_jogada}")
+            return carta_jogada
+        except (ValueError, IndexError):
+            print("Opção inválida. Tente novamente.")
+            return None
 
