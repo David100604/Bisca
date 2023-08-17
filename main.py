74eaf128ca681baf9ba14cf47cae98a7c5f7b2c0
David100604 <114587007+David100604@users.noreply.github.com>
2023-08-17 10:15:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,4 @@ from jogo import Jogo
 
 jogo1 = Jogo()
 
-jogo1.CriarJogo(4)
-jogo1.embaralhar()
-jogo1.Cortar()
-jogo1.distribuir_cartas(3)
-jogo1.MostrarCartas()
-jogo1.CriarDuplas()
-jogo1.DefinirComeco()
-for jogador in jogo1.jogadores:
-    print(jogador.nome)
-    print(jogador.vez)
-
-
-
+jogo1.Comecar()
