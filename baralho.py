d776f2a14f11ae865c677b8b61d5af3fba0c9375
David100604 <114587007+David100604@users.noreply.github.com>
2023-08-17 08:01:53 -0300

    Update baralho.py

--- baralho.py
+++ baralho.py
@@ -3,12 +3,13 @@ import random
 
 class Baralho:
     def __init__(self):
-        self.cartasC = ['2C', '3C', '4C', '5C', '6C', 'QC', 'JC', 'KC', '7C', 'AC']
-        self.cartasP = ['2P', '3P', '4P', '5P', '6P', 'QP', 'JP', 'KP', '7P', 'AP']
-        self.cartasO = ['2O', '3O', '4O', '5O', '6O', 'QO', 'JO', 'KO', '7O', 'AO']
-        self.cartasE = ['2E', '3E', '4E', '5E', '6E', 'QE', 'JE', 'KE', '7E', 'AE']
+        self.cartasC = ['2♥', '3♥', '4♥', '5♥', '6♥', 'Q♥', 'J♥', 'K♥', '7♥', 'A♥']
+        self.cartasP = ['2♣', '3♣', '4♣', '5♣', '6♣', 'Q♣', 'J♣', 'K♣', '7♣', 'A♣']
+        self.cartasO = ['2♦', '3♦', '4♦', '5♦', '6♦', 'Q♦', 'J♦', 'K♦', '7♦', 'A♦']
+        self.cartasE = ['2♠', '3♠', '4♠', '5♠', '6♠', 'Q♠', 'J♠', 'K♠', '7♠', 'A♠']
         self.cartas = self.cartasC + self.cartasP + self.cartasE + self.cartasO
         self.qtdeCartas = 40
+        self.biscas = ['7♠', 'A♠', '7♦', 'A♦', '7♣', 'A♣', '7♥', 'A♥']
 
     def consultar(self):
         print(self.cartas)
@@ -19,5 +20,4 @@ class Baralho:
     def MostrarCartas(self):
         for jogador in self.jogadores:
             print(f"{jogador.nome}: {jogador.mao}")
-
 
