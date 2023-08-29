3de3e3dc2d35fcc218e9a127278e44ea7422bfda
David100604 <114587007+David100604@users.noreply.github.com>
2023-08-29 11:14:35 -0300

    Update jogador.py

--- jogador.py
+++ jogador.py
@@ -20,10 +20,13 @@ class Jogador():
         print(f"{self.nome}, é a sua vez de jogar.")
         print("Suas cartas:", self.mao)
 
-        self.cartaJogada = input("Escolha uma carta para jogar (digite o índice da carta na sua mão): ")
+        self.cartaJogada = int(input("Escolha uma carta para jogar (digite o índice da carta na sua mão): "))
+
+        while self.cartaJogada > len(self.mao):
+            input("Escolha uma carta para jogar (Insira um número de 1 a {}): ".format(len(self.mao)))
 
         try:
-            carta_jogada_index = int(self.cartaJogada) - 1
+            carta_jogada_index = self.cartaJogada - 1
             self.cartaJogada = self.mao[carta_jogada_index]
             self.naipe = self.cartaJogada[1:]
             self.carta = self.cartaJogada[:1]
@@ -31,27 +34,32 @@ class Jogador():
             print(self.vez)
 
             while self.cartaJogada == self.sete:
-                if self.vez == 4:
-                    self.cartaJogada = input("Você não pode jogar está carta, escolha outra: ")
-                    carta_jogada_index = int(self.cartaJogada) - 1
-                    self.cartaJogada = self.mao[carta_jogada_index]
-                else:
-                    self.cartaJogada = self.sete
+                if self.a in self.mao:
                     break
-            else:
-                while self.cartaJogada == self.a:
-                    if self.sete not in self.cartasJogadas:
-                        self.cartaJogada = input("Você não pode jogar o Às do corte antes do 7, escolha outra: ")
+                else:
+                    if self.vez == 4:
+                        self.cartaJogada = input("Você não pode jogar está carta, escolha outra: ")
                         carta_jogada_index = int(self.cartaJogada) - 1
                         self.cartaJogada = self.mao[carta_jogada_index]
                     else:
-                        self.cartaJogada = self.a
+                        self.cartaJogada = self.sete
                         break
+            else:
+                while self.cartaJogada == self.a:
+                    if self.sete in self.mao:
+                        break
+                    else:
+                        if self.sete not in self.cartasJogadas:
+                            self.cartaJogada = input("Você não pode jogar o Às do corte antes do 7, escolha outra: ")
+                            carta_jogada_index = int(self.cartaJogada) - 1
+                            self.cartaJogada = self.mao[carta_jogada_index]
+                        else:
+                            self.cartaJogada = self.a
+                            break
 
             self.mao.pop(carta_jogada_index)
             return self.cartaJogada
         except (ValueError, IndexError):
             print("Opção inválida. Tente novamente.")
             return None
-
 
